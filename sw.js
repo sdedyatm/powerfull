d7e9dff11cad77f623abdbda12b696e379bc6988
sdedyatm <11027164+sdedyatm@users.noreply.github.com>
2026-02-09 22:28:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 const CACHE_NAME = "atm-pwa-v-" + Date.now();
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/gh/nextapps-de/flexsearch@0.7.31/dist/flexsearch.bundle.js",
-  "https://unpkg.com/dexie/dist/dexie.js"
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png', // Tambahkan ini
+  './icon-512.png', // Tambahkan ini
+  'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/gh/nextapps-de/flexsearch@0.7.31/dist/flexsearch.bundle.js',
+  'https://unpkg.com/dexie/dist/dexie.js'
 ];
 
 self.addEventListener("install", (e) => {
