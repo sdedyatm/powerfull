d097bca9ec8748b3fb71b462ce1341e2ada21b1f
sdedyatm <11027164+sdedyatm@users.noreply.github.com>
2026-02-10 20:49:40 +0700

    Update worker-search.js

--- worker-search.js
+++ worker-search.js
@@ -1,21 +1,40 @@
-importScripts("https://cdn.jsdelivr.net/gh/nextapps-de/flexsearch@0.7.31/dist/flexsearch.bundle.js");
+// worker.js
+importScripts(
+  "https://cdn.jsdelivr.net/gh/nextapps-de/flexsearch@0.7.31/dist/flexsearch.bundle.js"
+);
 
+// Inisialisasi index di dalam worker
 const index = new FlexSearch.Document({
   document: {
     id: "id",
     index: ["search_blob"],
     store: ["rowArray"]
   },
-  tokenize: "forward"
+  tokenize: "forward",
+  cache: true
 });
 
-self.onmessage = e => {
+self.onmessage = (e) => {
   const { type, payload } = e.data;
 
-  if (type === "ADD") payload.forEach(d => index.add(d));
-  if (type === "CLEAR") index.clear();
+  if (type === "ADD") {
+    // Tambahkan data ke index worker
+    payload.forEach((d) => index.add(d));
+    self.postMessage({ type: "STATUS", msg: "Data diindex oleh Worker" });
+  }
+
+  if (type === "CLEAR") {
+    index.clear();
+  }
+
   if (type === "SEARCH") {
-    const res = index.search(payload, { enrich: true, limit: 5000 });
-    self.postMessage(res);
+    // Limit hasil ke 50-100 agar transfer data ke Main Thread tidak LAG
+    const res = index.search(payload, {
+      enrich: true,
+      limit: 5000
+    });
+
+    // Kirim hasil kembali ke Main Thread
+    self.postMessage({ type: "RESULTS", data: res });
   }
 };
